6dcc7305c45e0ef273128a8fa564e617f4cd1e9a
Priya Nandi <priyamissu1999@gmail.com>
2020-12-29 10:29:00 +0530

    Navigation Drawer Layout

---
+++ app/src/main/java/com/example/mynotes/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.mynotes;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout= findViewById(R.id.drawerLayout);
+        navigationView= findViewById(R.id.navigationView);
+        toolbar= findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toggle= new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.setDrawerIndicatorEnabled(true);
+        toggle.syncState();
+    }
+}
